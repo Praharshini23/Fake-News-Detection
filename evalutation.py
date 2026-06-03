99853b91f23aa6a3d5d0c19dc7b1c55000931f7e
Praharshini Seth <s.praharshini05@gmail.com>
2026-06-03 12:41:28 +0530

    Create evalutation.py

---
+++ evalutation.py
@@ -0,0 +1,10 @@
+print("\nAccuracy:")
+print(accuracy_score(y_test, y_pred))
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+cm = confusion_matrix(y_test, y_pred)
+
+print("\nConfusion Matrix:")
+print(cm)
